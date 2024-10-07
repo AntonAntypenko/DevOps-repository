d5a125019bfd2b5a84b24151c4e72d56fdb3a0b8
DarinaVechtomova <134646815+DarinaVechtomova@users.noreply.github.com>
2024-10-07 19:17:08 +0200

    Тестування проекту і виправлення помилок

--- Lab_1/Lab_1/obj/Debug/net6.0/Lab_1.AssemblyInfo.cs
+++ Lab_1/Lab_1/obj/Debug/net6.0/Lab_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3005105de6522e8110fa23470a9c16b353d9203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
